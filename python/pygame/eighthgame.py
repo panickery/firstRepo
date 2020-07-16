504a6b116c75dfa2b44e26966ea0b383afbb9832
hj in work <hj@nowhere.com>
2020-07-16 16:10:50 +0900

    1.seventhgame 주석 추가 2. eighthgame 추가(Insertion sort)

--- python/pygame/eighthgame.py
+++ python/pygame/eighthgame.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import time
 
-# Selection sort
+# Insertion sort
 
 arr_cnt = 50
 screen_width = arr_cnt * 4
@@ -18,17 +18,26 @@ WHITE  = (255, 255, 255)
 font = None
 run = True
 
-# Selection Sort
+# Insertion sort
 def sort(arr) : 
-    for i in range(0, len(arr)-1) :
-        least = i
-        for j in range(i+1, len(arr)) :
-            if arr[least] > arr[j] :
-                least = j
-        if i != least :
-            temp = arr[j]
-            arr[j] = arr[i]
-            arr[i] = temp
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j - 1] > arr[j]:
+                arr[j - 1], arr[j] = arr[j], arr[j - 1]
+
+# 한번 부를때마다 다음 단계를 return 해야한다.
+def sortSelected(arr, i, j) :
+    if j == 0 :
+        i += 1
+        j = i
+    
+    if i == len(arr) - 1 : 
+        return False, arr, i, j
+
+    if arr[j - 1] > arr[j]:
+        arr[j - 1], arr[j] = arr[j], arr[j - 1]
+
+    return True, arr, i, j-1
 
 def drawArr(arr, selected) :
     for cnt in range(0, len(arr)) : 
@@ -37,26 +46,6 @@ def drawArr(arr, selected) :
             pygame.draw.rect(screen, PURPLE, pos, 2)    
         else :
             pygame.draw.rect(screen, WHITE, pos, 2)
-
-# 한번 부를때마다 다음 단계를 return 해야한다.
-def sortSelected(arr, i, j, least) :
-    if j == len(arr) :
-        if i != least :
-            temp = arr[i]
-            arr[i] = arr[least]
-            arr[least] = temp
-        i += 1
-        j = i
-        least = i
-        # i is in End. it has to be reset
-        if i == len(arr) - 1 : 
-            return False, arr, i, j, least
-        return True, arr, i, j, least
-
-    if arr[least] > arr[j] :
-        least = j
-
-    return True, arr, i, j+1, least
 
 def basicScreen(count) : 
     text = font.render(str(count), True, WHITE)
@@ -78,7 +67,7 @@ def runGame() :
     count = 0
     notEnd = False
     # Selection Sort에서 i, j, least에 해당하는 부분 그대로 사용
-    i, j, least = 0, 0, 0
+    i, j = 0, 0
 
     while run:
         for event in pygame.event.get():
@@ -87,16 +76,15 @@ def runGame() :
 
         if not notEnd :
             arr = resetArray()
-            i = 0
-            j = i+1
-            least = i
+            i = 1
+            j = i
             count = 0
 
         screen.fill(pygame.color.Color(0, gb[0], gb[1]))
         drawArr(arr, j)
         basicScreen(count)
 
-        notEnd, arr, i, j, least = sortSelected(arr, i, j, least)
+        notEnd, arr, i, j = sortSelected(arr, i, j)
         count+=1
 
     pygame.quit()
