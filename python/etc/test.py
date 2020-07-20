a9ac04ee4d0675e4de055b9ecca9f3cc9bd84567
hj in work <hj@nowhere.com>
2020-07-20 18:53:18 +0900

    update

--- python/etc/test.py
+++ python/etc/test.py
@@ -1,6 +1,11 @@
-import re
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        print("good")
 
-test = '!m@y# name  is good'
-test = test[1:]
-print(test)
-
+if __name__ == '__main__' :
+    tstr = "  good  is  bad  "
+    tstr_test = tstr.strip().split(' ')
+    tstr_test.reverse()
+    tstr_test = filter(lambda x : x != ' ', tstr_test)
+    tstr_test = " ".join(tstr_test)
+    print(tstr_test)
