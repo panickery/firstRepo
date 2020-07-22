def26b60e3ccb9ec020d0a1ba453b6f696d8696a
hanju in home <hanju@hanju.com>
2020-07-22 23:52:34 +0900

    leet code test

--- python/etc/test.py
+++ python/etc/test.py
@@ -1,11 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def reverseWords(self, s: str) -> str:
-        print("good")
+    def zigzagLevelOrder(self, root: TreeNode) :
+        arr_result = []
+        arr_tmp = getChild(root)
+        arr_result.append(arr_tmp)
+        arr_val = [arr_node[0].val, arr_node[1].val]
+        print(arr_val)
+
+def getChild(node) :
+    return [node.left, node.right]
+
+
 
 if __name__ == '__main__' :
-    tstr = "  good  is  bad  "
-    tstr_test = tstr.strip().split(' ')
-    tstr_test.reverse()
-    tstr_test = filter(lambda x : x != ' ', tstr_test)
-    tstr_test = " ".join(tstr_test)
-    print(tstr_test)
+    node1 = TreeNode(9)
+    node2 = TreeNode(15)
+    node3 = TreeNode(7)
+    node4 = TreeNode(20, node2, node3)
+    tree = TreeNode(3, node1, node4)
+    result = Solution()
+
+    print(result.zigzagLevelOrder(tree))
+
+
